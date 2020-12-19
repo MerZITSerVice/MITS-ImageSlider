c24037f43e2c9cd517844c705385d1232944cf05
Hetfield <hetfield@merz-it-service.de>
2020-12-19 01:48:54 +0100

    Add files via upload

--- shoproot/includes/extra/wysiwyg/mits_imageslider.php
+++ shoproot/includes/extra/wysiwyg/mits_imageslider.php
@@ -1,10 +1,20 @@
 <?php
-	// WYSIWYG editor Imageslider - (c) Copyright 2008-2016 by Hetfield - www.MerZ-IT-SerVice.de
-	// only original from https://www.merz-it-service.de/Installation-Imageslider-v1-5::74.html
-    switch($type) {
-        case 'imagesliders_description':
-            $editorName 			= 'imagesliders_description['.$langID.']';
-            $default_editor_height 	= 400;
-          	break;
-    }
-?>
+/**
+ * --------------------------------------------------------------
+ * File: mits_imageslider.php
+ * Date: 16.07.2020
+ * Time: 17:37
+ *
+ * Author: Hetfield
+ * Copyright: (c) 2020 - MerZ IT-SerVice
+ * Web: https://www.merz-it-service.de
+ * Contact: info@merz-it-service.de
+ * --------------------------------------------------------------
+ */
+
+switch ($type) {
+  case 'imagesliders_description':
+    $editorName = 'imagesliders_description[' . $langID . ']';
+    $default_editor_height = 400;
+    break;
+}
